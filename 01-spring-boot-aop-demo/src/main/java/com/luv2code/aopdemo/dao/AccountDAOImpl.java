0b665334a19e364403023919baf98ddae3a76579
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-03 22:38:19 +0100

    The method implementation

--- 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
+++ 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 public class AccountDAOImpl implements AccountDAO {
     @Override
     public void addAccount() {
-
+         System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT ");
     }
 }
