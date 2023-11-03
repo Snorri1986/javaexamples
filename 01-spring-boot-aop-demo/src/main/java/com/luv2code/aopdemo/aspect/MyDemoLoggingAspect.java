a1d6bc62d10eb262d938b16881f73b0cc055e6b3
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-03 22:52:03 +0100

    Define aspect type

--- 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,17 @@
 package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+
+    // this we will add all related advices for logging
+
+    @Before("execution(public void addAccount())") //aspect type
+    public void beforeAddAccountAdvice() {
+
+    }
 }
