e74277a75c8a0fceb202ebf915c94c7c5272b4b1
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-11-14 22:27:47 +0100

    AOP only for specific method

--- 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -11,7 +11,7 @@ public class MyDemoLoggingAspect {
     // this we will add all related advices for logging
 
     // execution is pointcut expression
-    @Before("execution(public void addAccount())") //aspect type
+    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())") //aspect type
     public void beforeAddAccountAdvice() {
         System.out.println("\n=======>>>>> Executing @Before advice on addAccount()");
     }
