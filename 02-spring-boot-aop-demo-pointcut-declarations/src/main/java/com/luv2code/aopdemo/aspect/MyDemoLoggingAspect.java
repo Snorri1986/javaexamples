e7d9140bbf713cec89abe9c23398088a475c590c
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-12-12 21:09:48 +0100

    Use pointcut declaration

--- 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,8 +11,11 @@ public class MyDemoLoggingAspect {
 
     // this we will add all related advices for logging
 
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
     // execution is pointcut expression
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))") //aspect type
+    @Before("forDaoPackage()") //aspect type
     public void beforeAddAccountAdvice() {
         System.out.println("\n=======>>>>> Executing @Before advice on addSillyMember()");
     }
